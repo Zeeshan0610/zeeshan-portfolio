db3d42e04a021997348a8205c68559f93d7bfdbe
Zeeshan <zeeshan.mukram6@gmail.com>
2025-06-11 19:53:12 +0530

    Added navbar for page navigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
@@ -8,6 +9,7 @@ import Contact from "./pages/Contact";
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
