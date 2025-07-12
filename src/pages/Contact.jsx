29e564a530c1db63142c0f37886a986fa363710b
Zeeshan <zeeshan.mukram6@gmail.com>
2025-07-12 17:59:19 +0530

    Fix target=_blank missing rel attribute

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,9 +7,9 @@ function Contact() {
       <p>Feel free to reach out via email or Call!</p>
 
       <div className="contact-links">
-        <p>Email: <a  href="mailto:Zeeshan.mukram6@gmail.com" target="_blank">Zeeshan.mukram6@gmail.com</a></p>
+        <p>Email: <a  href="mailto:Zeeshan.mukram6@gmail.com" target="_blank" rel="noopener noreferrer">Zeeshan.mukram6@gmail.com</a></p>
         <p>Mobile No(WhatsApp): 9066852215</p>
-        <p>LinkedIn: <a href="https://linkedin.com/in/mohammed-zeeshan-mukram-348489260/" target="_blank">Mohammed Zeeshan Mukram</a></p>
+        <p>LinkedIn: <a href="https://linkedin.com/in/mohammed-zeeshan-mukram-348489260/" target="_blank" rel="noopener noreferrer">Mohammed Zeeshan Mukram</a></p>
       </div>
     </div>
   );
